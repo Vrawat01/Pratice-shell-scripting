4713fd2beba16ce327b28d45a4b934aac68fa075
Vikas rawat <vikas99.rawat@gmail.com>
2022-11-20 17:25:38 +0530

    frontendniginx

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -4,7 +4,7 @@ yum install nginx -y
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
-rm -rf /usr/share/nginx/html
+rm -rf /usr/share/nginx/html/*
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 # mv frontend-main/* .
